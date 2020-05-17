ac6c0d8b15c7f7e6a3168b9762c206c90c257153
Андрей Пугачев <apugachevdev@gmail.com>
2020-05-18 00:08:41 +0300

    New project structure

---
+++ include/qtthreadmessagedispatcher.h
@@ -0,0 +1,6 @@
+#include <../src/irequest.h>
+#include <../src/iresponse.h>
+#include <../src/handler_registry.h>
+#include <../src/requester_wrapper.h>
+#include <../src/thread_manager.h>
+#include <../src/thread_message_dispatcher.h>
